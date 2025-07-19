c4c2ae332a3bbf16a7bb07b3bd22bd94462f4d63
Andrew Flis <andrewef2000@gmail.com>
2025-07-19 18:16:55 -0400

    vid9

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,18 +3,12 @@
 
 int main(){
     
-    const float PI = 3.14159265359;
-    double pi2 = 3.14159265359;
-    printf("float:\t%0.15f\n", PI);
-    printf("double:\t%0.15lf\n", pi2);
+    int z = 1;
+    z+= 2;
+    z++;
 
-    bool fact = true;
-    printf("bool:\t%d\n", fact);
-
-    char letter = 100;
-    unsigned char letter2 = 255;
-    printf("char:\t%c\n", letter); 
-    printf("char2:\t%d\n", letter2); 
+    // print value
+    printf("%d", z);
 
     return 0;
 }
