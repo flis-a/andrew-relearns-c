824ae12564c2cbbbf2e11e6cb205fbcea21f1f41
Andrew Flis <andrewef2000@gmail.com>
2025-07-20 14:10:39 -0400

    vid26

--- HelloWorld.c
+++ HelloWorld.c
@@ -4,20 +4,32 @@
 #include <math.h>
 #include <ctype.h>
 
-void hello(char[], int);
-
 int main(){
 
-    char name[] = "Andrew";
-    int age = 24;
+    char s1[] = "Andrew";
+    char s2[] = "aBanana";
+    int num = 4;
 
-    hello(name, age);
+    //strlwr(s1);
+    //strupr(s2);
+    //strcat(s1,s2);
+    //strncat(s1,s2,num);
+    //strcpy(s1,s2);
+    //strncpy(s1,s2,num);
+
+    //strset(s1,'*');
+    //strnset(s1,'*',1);
+    //strrev(s2);
+
+    //num = strlen(s1);
+    //num = strcmp(s1,s2);
+    //num = strncmp(s1,s2, 1);
+    //num = strcmpi(s1,s2);
+    num = strnicmp(s1,s2,1);
+
+    printf("\n%s",s1);
+    printf("\n%s",s2);
+    printf("\n%d",num);
 
     return 0;
-}
-
-void hello(char name[], int age)
-{
-    printf("\n%s",name);
-    printf("\n%d",age);
 }
