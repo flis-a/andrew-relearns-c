2b194b4a494c97721cd867e43e29c111a350ccc0
Andrew Flis <andrewef2000@gmail.com>
2025-07-20 14:18:15 -0400

    vid27

--- HelloWorld.c
+++ HelloWorld.c
@@ -6,30 +6,15 @@
 
 int main(){
 
-    char s1[] = "Andrew";
-    char s2[] = "aBanana";
-    int num = 4;
+    int num;
 
-    //strlwr(s1);
-    //strupr(s2);
-    //strcat(s1,s2);
-    //strncat(s1,s2,num);
-    //strcpy(s1,s2);
-    //strncpy(s1,s2,num);
-
-    //strset(s1,'*');
-    //strnset(s1,'*',1);
-    //strrev(s2);
-
-    //num = strlen(s1);
-    //num = strcmp(s1,s2);
-    //num = strncmp(s1,s2, 1);
-    //num = strcmpi(s1,s2);
-    num = strnicmp(s1,s2,1);
-
-    printf("\n%s",s1);
-    printf("\n%s",s2);
-    printf("\n%d",num);
+    printf("Enter num: ");
+    scanf("%d", &num);
+    
+    for(int i = 0; i <= num; i+=2)
+    {
+        printf("\nNum:\t%d",i);
+    }
 
     return 0;
 }
