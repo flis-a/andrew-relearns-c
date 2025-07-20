b7452de73d341aaf0e8bb5fe947880421bd04f74
Andrew Flis <andrewef2000@gmail.com>
2025-07-20 16:09:32 -0400

    vid35

--- HelloWorld.c
+++ HelloWorld.c
@@ -6,35 +6,15 @@
 
 int main(){
 
-    /*
-    int num[2][3] = {
-                        {1, 2, 3},
-                        {4, 5, 6}
-                    };
-    */
+    char cars[][10] = {"Mustang","Corvette","Porche"};
 
-    int num[2][3];
-    int row = sizeof(num)/sizeof(num[0]);
-    int col = sizeof(num[0])/sizeof(num[0][0]);
+    strcpy(cars[0], "Telsa");
 
-    num[0][0] = 1;
-    num[0][1] = 2;
-    num[0][2] = 3;
-    num[1][0] = 4;
-    num[1][1] = 5;
-    num[1][2] = 6;
+    for(int i = 0; i < sizeof(cars)/sizeof(cars[0]); i++){
 
-    for(int i = 0; i<row; i++){
-
-        for(int j = 0; j<col; j++){
-            
-            printf("%d ",num[i][j]);
-
-        }
-        printf("\n");
+        printf("%s\n",cars[i]);
 
     }
-
     
     return 0;
 }
