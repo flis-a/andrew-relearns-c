aee06295a9fc6c07ccf7eb4039cccbab6288a33c
Andrew Flis <andrewef2000@gmail.com>
2025-07-23 19:44:31 -0400

    vid40

--- HelloWorld.c
+++ HelloWorld.c
@@ -4,23 +4,25 @@
 #include <math.h>
 #include <ctype.h>
 
-//typedef char user[25];
-
 typedef struct
 {
-    char name[25];
-    char password[32];
-    int id;
+    char name[15];
+    float GPA;
 
-}User;
+}Student;
 
 int main(){
 
-    User user1 = {"Andrew","123",123456789};
-    User user2 = {"Banana","456",123456789};
+    Student s1 = {"John Wick",4.0};
+    Student s2 = {"Peter Griffin",2.2};
+    Student s3 = {"Spongebob",3.4};
+    Student s4 = {"Batman",5.0};
 
-    printf("%s\n",user1.name);
-    printf("%s\n",user2.name);
+    Student students[] = {s1,s2,s3,s4};
+
+    for(int i = 0; i < sizeof(students)/sizeof(students[1]); i++){
+        printf("%-15s\t%0.2f\n",students[i].name,students[i].GPA);
+    }
 
     return 0;
 }
