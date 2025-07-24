56537b6c0ad0442fbd758c07d610228c30ab3e0e
Andrew Flis <andrewef2000@gmail.com>
2025-07-23 23:58:22 -0400

    vid44 - Still need to troubleshoot scanf("%c")

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,43 +1,60 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <ctype.h>
 
 int main(){
 
-    const int MIN = 1;
-    const int MAX = 100;
-    int guess;
-    int guesses = 0;
-    int answer;
+    char questions[][100] = 
+    {"1. 1+1=?",
+     "2. 1*1=?",
+     "3. 1/1=?"
+    };
     
-    srand(time(0));
+    char options[][100] = 
+    {"A. 1","B. 2","C. 0","D. 10",
+     "A. 1","B. 10","C. 100","D. 1000",
+     "A. 1","B. 10","C. 100","D. 1000",
+    };
+    char answers[3] = {'B','A','A'};
+    int numOfQuestions = sizeof(questions)/sizeof(questions[0]);
 
-    answer = (rand() % MAX) + MIN;
+    char guess;
+    int score = 0;
 
-    do{
+    printf("MATH TEST:\n");
+
+    for(int i = 0; i < numOfQuestions; i++){
+        
+        printf("%s\n",questions[i]);
+        printf("~~~~~~~~~~~~\n");
+
+        for(int j = (i*4); j < i*4 +4; j++ ){
+
+            printf("%s\t",options[j]);
+
+        }
+        printf("\n~~~~~~~~~~~~\n");
 
         printf("Guess: ");
-        scanf("%d",&guess);
+        scanf("%c", &guess);
+        printf("%c"); //figure out why this does work
 
-        if(guess > answer){
+        guess = toupper(guess);
 
-            printf("To High!\n");
+        if(guess == answers[i]){
 
-        }else if(guess < answer){
-
-            printf("To Low!\n");
+            printf("Correct, +1\n");
+            score++;
 
         }else{
 
-            printf("WINNER!\n");
+            printf("Wrong\n");
 
         }
+        
 
-        guesses++;
+    }
 
-    }while(guess != answer);
-
-    printf("You guessed %d, in %d tries!\n",answer,guesses);
+    printf("Final Score: %d/%d\n", score, numOfQuestions);
 
     return 0;
 }
