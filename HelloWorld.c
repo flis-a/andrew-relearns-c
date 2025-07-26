915b27ca9017e09d8d9be537f4a0a29337dad8f5
Andrew Flis <andrewef2000@gmail.com>
2025-07-26 12:19:03 -0400

    vid 45

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,58 +3,24 @@
 
 int main(){
 
-    char questions[][100] = 
-    {"1. 1+1=?",
-     "2. 1*1=?",
-     "3. 1/1=?"
-    };
+    int x = 6;  // = 00000110
+    int y = 12; // = 00001100
+    int z = 0;  // = 00000000
+
+    z = x & y;
+    printf("AND: %d\n",z);
+
+    z = x | y;
+    printf("OR: %d\n",z);
+
+    z = x ^ y;
+    printf("XOR: %d\n",z);
+
+    z = x << 1;
+    printf("SHIFT LEFT: %d\n",z);
+
+    z = x >> 1;
+    printf("SHIFT RIGHT: %d\n",z);
     
-    char options[][100] = 
-    {"A. 1","B. 2","C. 0","D. 10",
-     "A. 1","B. 10","C. 100","D. 1000",
-     "A. 1","B. 10","C. 100","D. 1000",
-    };
-    char answers[3] = {'B','A','A'};
-    int numOfQuestions = sizeof(questions)/sizeof(questions[0]);
-
-    char guess;
-    int score = 0;
-
-    printf("MATH TEST:\n");
-
-    for(int i = 0; i < numOfQuestions; i++){
-        
-        printf("%s\n",questions[i]);
-        printf("~~~~~~~~~~~~\n");
-
-        for(int j = (i*4); j < i*4 +4; j++ ){
-
-            printf("%s\t",options[j]);
-
-        }
-        printf("\n~~~~~~~~~~~~\n");
-
-        printf("Guess: ");
-        scanf("%c", &guess);
-        scanf("%*c");
-
-        guess = toupper(guess);
-
-        if(guess == answers[i]){
-
-            printf("Correct, +1\n");
-            score++;
-
-        }else{
-
-            printf("Wrong\n");
-
-        }
-        
-
-    }
-
-    printf("Final Score: %d/%d\n", score, numOfQuestions);
-
     return 0;
 }
