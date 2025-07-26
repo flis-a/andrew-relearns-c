b2abda2251d8e9862e7eb33144c7c39e4ae7cc84
Andrew Flis <andrewef2000@gmail.com>
2025-07-26 14:41:42 -0400

    vid50

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,21 +1,226 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+char board[3][3];
+const char PLAYER = 'O';
+const char COMPUTER = 'X';
+
+int record[3] = {0,0,0};
+
+void resetBoard();
+void printBoard();
+int checkFreeSpaces();
+void playerMove();
+void computerMove();
+char checkWinner();
+void printWinner(char);
+void incrementRecord(char);
 
 int main(){
     
-    FILE *pF = fopen("words.txt", "r");
-    char buffer[255];
+    char winner = ' ';
+    char response;
 
-    if(pF == NULL){
-        printf("UNABLE TO OPEN");
-    }else{
-        while(fgets(buffer, 255, pF) != NULL)
-            {
-                printf("%s", buffer);
-            }
+    do{
+        winner = ' ';
+        response = ' ';
+        resetBoard();
+
+    while(winner == ' ' && checkFreeSpaces() != 0){
+        // PLAYER
+        printBoard();
+        playerMove();
+        winner = checkWinner();
+        if(winner != ' ' || checkFreeSpaces() == 0){
+            break;
+        }
+
+        // COMPUTER
+        computerMove();
+        winner = checkWinner();
+        if(winner != ' ' || checkFreeSpaces() == 0){
+            break;
+        }
+
     }
 
+    // Print Results
+    printBoard();
+    printWinner(winner);
+    incrementRecord(winner);
 
+    // Ask to play again
+    printf("\nPlay Again? (Y/N): ");
+    scanf("%*c");
+    scanf("%c",&response);
+    response = toupper(response);
 
-    fclose(pF);
+    } while (response == 'Y');
+    
     return 0;
+}
+
+void resetBoard(){
+
+    // Clears board
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            board[i][j] = ' ';
+        }
+    }
+
+}
+
+void printBoard(){
+    
+    // Prints current board state
+    printf(" %c | %c | %c ",board[0][0],board[0][1],board[0][2]);
+    printf("\n---|---|---\n");
+    printf(" %c | %c | %c ",board[1][0],board[1][1],board[1][2]);
+    printf("\n---|---|---\n");
+    printf(" %c | %c | %c ",board[2][0],board[2][1],board[2][2]);
+    printf("\n");
+
+}
+
+int checkFreeSpaces(){
+
+    int freeSpaces = 9;
+
+    // Checks Board
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            if(board[i][j] != ' '){
+                freeSpaces--;
+            }
+            
+        }
+    }
+
+    return freeSpaces;
+
+}
+
+void playerMove(){
+
+    int x;
+    int y;
+
+    do{
+        printf("Enter row #: ");
+        scanf("%d",&x);
+        x--;
+        printf("Enter column #: ");
+        scanf("%d",&y);
+        y--;
+        if(y > 2 || x > 2 || y < 0 || x < 0){
+            printf("OUT OF BOUNDS!\n");
+        }else if(board[x][y] != ' '){
+            printf("Invalid move!\n");
+        }else{
+            board[x][y] = PLAYER;
+            break;
+        }
+
+    }while(board[x][y] != ' ');
+
+}
+
+void computerMove(){
+    // Create seed
+    srand(time(0));
+    int x;
+    int y;
+
+    if(checkFreeSpaces() > 0){
+
+        do{
+            x = rand() % 3;
+            y = rand() % 3;
+
+        }while(board[x][y] != ' ');
+
+        board[x][y] = COMPUTER;
+
+    }else{
+        printWinner(' ');
+    }
+
+}
+
+char checkWinner(){
+
+    // Row Check
+    for(int i = 0; i < 3; i++){
+
+        if(board[i][0] == board[i][1] && board[i][0] == board[i][2]){
+            return board[i][0];
+        } 
+    }  
+
+    // Column Check
+    for(int i = 0; i < 3; i++){
+
+        if(board[0][i] == board[1][i] && board[0][i] == board[2][i]){
+            return board[0][i];
+        } 
+    }  
+
+    // Diagonal Check
+    /*
+     ! |   |   
+    ---|---|---
+       | ! |
+    ---|---|---
+       |   | !
+    */
+    if(board[0][0] == board[1][1] && board[0][0] == board[2][2]){
+            return board[0][0];
+    } 
+    /*
+       |   | ! 
+    ---|---|---
+       | ! |
+    ---|---|---
+     ! |   |  
+    */
+    if(board[0][2] == board[1][1] && board[0][2] == board[2][0]){
+            return board[0][2];
+    } 
+
+    return ' ';
+
+}
+
+void printWinner(char winner){
+
+    if(winner == PLAYER){
+
+        printf("\nYou Win!\n");
+
+    }else if(winner == COMPUTER){
+    
+        printf("\nYou Lose.\n");
+
+    }else{
+
+        printf("\nDRAW\n");
+
+    }
+}
+
+void incrementRecord(char winner){
+
+    if(winner == PLAYER){
+        record[0]++;
+    }else if(winner == COMPUTER){  
+        record[1]++;
+    }else{
+        record[2]++;
+    }
+
+    printf("Current Record: %d-%d-%d\n",record[0],record[1],record[2]);
+
 }
