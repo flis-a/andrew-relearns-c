ccc1125141a534dc2edd4587b6303e9ee8732591
Andrew Flis <andrewef2000@gmail.com>
2025-07-26 12:47:59 -0400

    vid48

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
 
-void printAge(int *pAge){
-
-    printf("You are %d years old\n",*pAge);
-
-}
-
 int main(){
+    
+    FILE *pF = fopen("test.txt", "w");
+    fprintf(pF, "test123\n");
+    fclose(pF);
 
-    int age = 21;
-    int *pAge = NULL;
-    pAge = &age;
-
-    printf("address of age: %p\n", &age);
-    printf("value of pAge: %p\n", pAge);
-
-    printf("size of age: %d bytes\n", sizeof(age));
-    printf("size of pAge: %d bytes\n", sizeof(pAge));
-
-    printf("value of age: %d\n", age);
-    printf("value at stored address: %d\n", *pAge);
-
-    printf("\n");
-
-    printAge(pAge);
+    /*
+    if(remove("test.txt") == 0)
+    {
+        printf("successfully deleted");
+    }else
+    {
+        printf("fail");
+    }
+    */
 
     return 0;
 }
